08be0d234e95cef45bc0c1ded82b2847552e1637
Christoph <christoph@debian>
2022-08-19 18:18:40 +0200

    :construction: create basic server setup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
-import fs from "fs"
-import path from "path"
+import fs                       from "fs"
+import path                     from "path"
+
+import express                  from 'express';
+import { ApolloServer, gql }    from 'apollo-server-express';
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-import * as jose from 'jose'
-import resolvers from "./resolvers";
-import express from 'express';
-const cookieParser = require('cookie-parser');
-let { ApolloServer, gql } = require('apollo-server-express');
+import { PrismaClient }         from '@prisma/client'
 
+import {
+  encrypt_user_id,
+  decrypt_user_id
+}                               from "./utils/jwt_helpers";
+import resolvers                from "./resolvers";
+
+const app = express();
 
 let typeDefs = fs.readFileSync(
   path.join(__dirname, 'schema.graphql'),
   'utf8'
 )
 
-let context = ({ req,res }) => {
-  //get the auth token
-  //let auth_token = req.headers["authorization"]
+let context = async ({ req,res }) => {
+  let prisma = new PrismaClient();
 
-  res.cookie('cookieName',123, { maxAge: 900000});
+  //get the auth token
+  let auth_token = req.headers["authorization"]
+
+  //get the user id
+  let user_id = await decrypt_user_id(auth_token);
+  if(!user_id) return {prisma:prisma,logged_in_user:null}
+
+  //get the logged in user with the id from the jwt
+  let user = await prisma.user.findUnique({where:{id:user_id}})
+
+  return {prisma: prisma,logged_in_user:user}
 }
 
 //let schema = makeExecutableSchema({
@@ -29,51 +44,15 @@ let context = ({ req,res }) => {
 //schema = constraintDirective()(schema)
 
 
-let get_user_id_of_logged_in_user = async () => {
-  let publicKey = await jose.importSPKI(fs.readFileSync(
-    path.join(__dirname, './../public.key'),
-    'utf8'
-  ), "RS256")
-
-  let privateKey = await jose.importPKCS8(fs.readFileSync(
-    path.join(__dirname, './../private.key'),
-    'utf8'
-  ), "RS256")
-
-  //login
-  const token = await new jose.SignJWT({
-    user_id: 1,
-  })
-  .setProtectedHeader({
-    typ: 'JWT',
-    alg: 'RS256',
-  })
-  .setExpirationTime('1h')
-  .sign(privateKey);
-  console.log(token)
-
-  try{
-    let { payload, protectedHeader } = await jose.jwtVerify(token+"k", publicKey);
-    return payload.user_id
-  }catch(error){
-    return null;
-  }
-}
-const app = express();
-
-app.use(cookieParser());
-
 //get_user_id_of_logged_in_user();
 let server = new ApolloServer({ typeDefs, resolvers, context });
 
-const cors = {
-  credentials: true,
-  origin: 'https://studio.apollographql.com'
-}
 
 let startServer= async () =>  {
   await server.start();
-  server.applyMiddleware({app,cors})
+  server.applyMiddleware({app})
 }
 startServer()
 app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+
+//TODO: Hinzufügen von LOGIN/REGISTER/LOGOUT MUTATIONS
